8edc6ead1429428c41212ab63c3d7b19c0290eb5
koushik-ms <koushik.ms@gmail.com>
2019-12-30 03:00:12 +0530

    algorithm implemented. all tests pass

--- tdd-samples/prime-factors/src/kata.cpp
+++ tdd-samples/prime-factors/src/kata.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 /*
     Prime factors kata
@@ -11,9 +11,9 @@ bool isDivisible(unsigned dividend, unsigned divisor) { return (dividend % divis
 vector<unsigned> primeFactors(unsigned number) {
     std::vector<unsigned> result{};
     unsigned limit = number / 2;
-    for(unsigned const& factor: {2, 3, 5}) {
-        if(factor > limit) break;
+    for(unsigned int factor = 2; factor <= limit; ++factor) {
         if(isDivisible(number, factor)) result.push_back(factor);
+        while(isDivisible(number, factor)) number /= factor;
     }
     return result;
 }
@@ -32,50 +32,30 @@ std::ostream& operator<< (std::ostream &os, vector<T> const& vec) {
 #include "doctest.h"
 
 TEST_CASE("Prime factors single digit numbers") {
-    SUBCASE("Prime Factors of 1 is empty list") {
-        auto x = primeFactors(1);
-        auto expected = std::vector<unsigned>();
-        CHECK(x == expected);
-    }
-    SUBCASE("Prime Factors of 2 is empty list") {
-        auto x = primeFactors(2);
-        auto expected = std::vector<unsigned>();
-        CHECK(x == expected);
-    }
-    SUBCASE("Prime Factors of 4 is {2}") {
-        auto expected = std::vector<unsigned>{{2}};
-        auto x = primeFactors(4);
-        CHECK(x == expected);
-    }
-    SUBCASE("Prime Factors of 5 is empty list") {
-        auto x = primeFactors(5);
-        auto expected = std::vector<unsigned>();
-        CHECK(x == expected);
-    }
-    SUBCASE("Prime Factors of 6 is {2, 3}") {
-        auto expected = std::vector<unsigned>{{2, 3}};
-        auto x = primeFactors(6);
-        CHECK(x == expected);
-    }
-    SUBCASE("Prime Factors of 9 is {3}") {
-        auto expected = std::vector<unsigned>{{3}};
-        auto x = primeFactors(9);
-        CHECK(x == expected);
-    }
+    CHECK(primeFactors(1) ==  std::vector<unsigned>());
+    CHECK(primeFactors(2) ==  std::vector<unsigned>());
+    CHECK(primeFactors(4) ==  std::vector<unsigned>{{2}});
+    CHECK(primeFactors(5) ==  std::vector<unsigned>());
+    CHECK(primeFactors(6) ==  std::vector<unsigned>{{2, 3}});
+    CHECK(primeFactors(8) ==  std::vector<unsigned>{{2}});
+    CHECK(primeFactors(9) ==  std::vector<unsigned>{{3}});
 }
 
 TEST_CASE("Prime Factors two digit numbers") {
-    SUBCASE("Prime factors of 10 is {2, 5}") {
-        auto expected = std::vector<unsigned>{{2, 5}};
-        auto x = primeFactors(10);
-        std::cout << expected << std::endl;
-        std::cout << x << std::endl;
-        CHECK(x == expected);
-    }
+    CHECK(primeFactors(10) ==  std::vector<unsigned>{{2, 5}});
+    CHECK(primeFactors(18) ==  std::vector<unsigned>{{2, 3}});
+    CHECK(primeFactors(27) ==  std::vector<unsigned>{{3}});
+    CHECK(primeFactors(30) ==  std::vector<unsigned>{{2, 3, 5}});
+    CHECK(primeFactors(76) ==  std::vector<unsigned>{{2, 19}});
+}
+
+TEST_CASE("Prime Factors larger numbers") {
+    CHECK(primeFactors( 128) == std::vector<unsigned>{{2}});
+    CHECK(primeFactors( 210) == std::vector<unsigned>{{2, 3, 5, 7}});
+    CHECK(primeFactors(2310) == std::vector<unsigned>{{2, 3, 5, 7, 11}});
 }
 
 TEST_CASE("Test kata" * doctest::skip(true)) {
-    CHECK(true);
     vector<int> v{1, 2, 3};
     vector<int> x = {1, 2, 3, 4};
     std::cout << v << std::endl;
