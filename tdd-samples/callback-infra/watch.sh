523c9e9cd650b6afd577466ead1554fc5f0cf260
koushik-ms <koushik.ms@gmail.com>
2020-03-19 15:41:44 +0100

    watch.sh uses local rerun,creates common build output directory at repo root

--- tdd-samples/callback-infra/watch.sh
+++ tdd-samples/callback-infra/watch.sh
@@ -1,1 +1,7 @@
-rerun -c -v bash -c "cd build && make && make CTEST_OUTPUT_ON_FAILURE=1 test"
+# A shell-wrapper to trigger build and execute whenever any file in this tree changes. 
+# Use during development for rapid feedback.
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+WATCH_DIR=$PWD
+bash -c "mkdir -p $REPO_ROOT/build && cd $REPO_ROOT/build && cmake $WATCH_DIR"
+$REPO_ROOT/bin/rerun -c -v bash -c "cd $REPO_ROOT/build && make && make CTEST_OUTPUT_ON_FAILURE=1 test"
