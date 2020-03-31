404db733c627fb830811dcb3ef2e82d699932bc4
koushik-ms <koushik.ms@gmail.com>
2020-03-31 17:03:46 +0200

    Learning tests for ID, track registrations with id
    Add learning tests for making an id for a registration. Keep track of
    registrations using the ID.

--- tdd-samples/callback-infra/int/main.cpp
+++ tdd-samples/callback-infra/int/main.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <thread>
 
-#include "solution.h"
-
 #include "implementation.h"
+#include "solution.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
@@ -37,20 +36,31 @@ TEST_CASE("What (de)Register is made-up of") {
   //     dependency Call register on sut, expect call to worker with right
   //     param values. Call de-register on sut, expect call to cancel.
 }
+
 TEST_CASE("How to keep track of registrations") {
   using namespace std::chrono_literals;
-  auto period = 500ms;
-  auto callback = []() {};
-  Infomap m;
-  Worker original;
+  auto one_sec = Duration{1s};
+  auto half_sec = Duration{500ms};
+  auto callback_fn = []() { std::cout << "Wonder!" << std::endl; };
+  std::map<CallbackInfrastructure::IdType, Worker> data_store;
 
-  original.schedule(period, callback);
-  auto id = m.add(original);
-  CHECK(id != 0);
+  Worker original_half;
+  original_half.schedule(half_sec, callback_fn);
+  CallbackInfrastructure::IdType id = MakeId(half_sec, callback_fn);
+  data_store.insert({id, original_half});
+  REQUIRE(data_store.find(id) != data_store.end());
+  CHECK(data_store.find(id)->second == original_half);
 
-  Worker retrieved = m.remove(id);
-  CHECK(retrieved == original);
+  Worker original_one;
+  original_one.schedule(one_sec, callback_fn);
+  id = MakeId(one_sec, callback_fn);
+  data_store.insert({id, original_one});
+
+  CHECK(original_one != original_half);
+  REQUIRE(data_store.find(id) != data_store.end());
+  CHECK(data_store.find(id)->second == original_one);
 }
+
 TEST_SUITE_END();  // "CallbackInfra integration scenario"
 
 TEST_SUITE_BEGIN("CallbackInfrastructureImpl Unit tests" *
@@ -82,7 +92,7 @@ class CallbackInfraShould {
   ~CallbackInfraShould() { delete sut_; }
 
  protected:
-  CallbackInfrastructure *sut_;
+  CallbackInfrastructure* sut_;
 };
 unsigned CallbackInfraShould::MockWorkerImpl::instanceCount{0};
 
@@ -96,6 +106,16 @@ TEST_CASE_FIXTURE(CallbackInfraShould,
   sut_->registerCallback(100ms, []() {});
   CHECK(CallbackInfraShould::MockWorkerImpl::instanceCount > 0);
 }
+TEST_CASE("CallbackInfra: register delegates correctly to worker schedule") {}
+TEST_CASE("register returns diff id for diff period same callback") {}
+TEST_CASE("register returns diff id for same period diff callback") {}
+TEST_CASE("allow de-register of callback") {}
+TEST_CASE("disallow de-register of invalid callback") {}
+TEST_CASE(
+    "silently fail when de-registering a callback that is already "
+    "de-registered") {}
+TEST_CASE("CallbackInfra shoud de-register all callbacks when destroyed") {}
+TEST_CASE("register shall return within 1ms") {}
 TEST_SUITE_END();  // CallbackInfrastructureImpl Unit tests
 
 TEST_SUITE_BEGIN("Worker Unit tests" * doctest::skip(!TEST_WORKER));
@@ -122,7 +142,8 @@ class WorkerShould {
   Duration arbitrary_wait_time;
 };
 
-TEST_CASE_FIXTURE(WorkerShould, "schedule a callback with local lambda" * doctest::timeout(2.0)) {
+TEST_CASE_FIXTURE(WorkerShould, "schedule a callback with local lambda" *
+                                    doctest::timeout(2.0)) {
   // TODO: Need a timeout on this test-case not to exceed 2.5 periods
   auto worker = WorkerImpl{};
   unsigned called{0};
@@ -136,7 +157,8 @@ TEST_CASE_FIXTURE(WorkerShould, "schedule a callback with local lambda" * doctes
 }
 
 TEST_CASE_FIXTURE(WorkerShould,
-                  "schedule a callback with free function/ class member func" * doctest::timeout(2.0)) {
+                  "schedule a callback with free function/ class member func" *
+                      doctest::timeout(2.0)) {
   MockCallbackProvider mock;
   auto worker = WorkerImpl{};
   auto memberFunc = std::bind(&MockCallbackProvider::doSomething, &mock);
@@ -149,7 +171,7 @@ TEST_CASE_FIXTURE(WorkerShould,
 }
 
 TEST_CASE_FIXTURE(WorkerShould, "cancel the currently schedule callback") {
-    auto worker = WorkerImpl{};
+  auto worker = WorkerImpl{};
   unsigned called{0};
   auto fn = [&called]() {
     std::cout << "Callback!" << std::endl;
@@ -162,10 +184,10 @@ TEST_CASE_FIXTURE(WorkerShould, "cancel the currently schedule callback") {
   worker.cancel();
   std::this_thread::sleep_for(arbitrary_period);
   CHECK(called <= total_called + 1);
-  
 }
 
-TEST_CASE_FIXTURE(WorkerShould, "invoke Callback repeatedly until cancelled" * doctest::timeout(3.0)) {
+TEST_CASE_FIXTURE(WorkerShould, "invoke Callback repeatedly until cancelled" *
+                                    doctest::timeout(3.0)) {
   auto worker = WorkerImpl{};
   unsigned called{0};
   unsigned multiple{3};
@@ -183,9 +205,90 @@ TEST_CASE_FIXTURE(WorkerShould,
   using namespace std::chrono_literals;
   auto worker = WorkerImpl{};
   auto before = Now();
-  worker.schedule(arbitrary_period, [](){});
+  worker.schedule(arbitrary_period, []() {});
   auto after = Now();
-  CHECK( (after-before) <= 1ms);
+  CHECK((after - before) <= 1ms);
 }
 
 TEST_SUITE_END();  // "Worker Unit Tests"
+
+// These are learning tests. By definition, this is bottom-up exploration of the
+// libraries, language features that are used to engineer the solutions used to
+// make the solution work.
+
+#include <chrono>
+#include <functional>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+
+TEST_SUITE_BEGIN("Learning tests");
+TEST_CASE("Making an idtype for identifying registrations") {
+  SUBCASE("Can make hash of a string") {
+    std::string arbitrary_string{"Some text"};
+    auto hasher = std::hash<std::string>{};
+    auto the_hash = hasher(arbitrary_string);
+    std::cout << "The has of " << arbitrary_string << " is: " << the_hash
+              << std::endl;
+  }
+  SUBCASE("two strings have different hashes") {
+    std::string first{"Some text1"};
+    std::string second{"Some text2"};
+    REQUIRE(first != second);
+
+    auto hasher = std::hash<std::string>{};
+    auto first_hash = hasher(first);
+    auto second_hash = hasher(second);
+
+    CHECK(first_hash != second_hash);
+  }
+  SUBCASE("hash of string is size_t") {
+    auto hasher = std::hash<std::string>{};
+    auto the_hash = hasher("Some string");
+    std::cout << "The hash of \"Some string\" is :" << the_hash << std::endl;
+    static_assert(std::is_same_v<std::size_t, decltype(the_hash)>,
+                  "hasher output is not size_t");
+  }
+  using namespace std::chrono_literals;
+  SUBCASE("Can convert a period and lambda into a string") {
+    Duration d{500ms};
+    auto f1 = []() {};
+    auto rep = std::to_string(d.count()) + typeid(f1).name();
+    static_assert(std::is_same_v<std::string, decltype(rep)>);
+    CHECK(!rep.empty());
+  }
+  SUBCASE("Can convert unique period and lambda into unique hash") {
+    Duration d1{500ms};
+    Duration d2{200ms};
+    Duration d3{500us};
+    auto f1 = []() {};
+    auto f2 = []() {};
+    auto f3 = []() { std::cout << "a very important action\n"; };
+    auto hasher = std::hash<std::string>{};
+    auto make_hash = [&hasher](auto a, auto b) -> std::size_t {
+      return hasher(std::to_string(a.count()) + typeid(b).name());
+    };
+
+    auto d1f1 = make_hash(d1, f1);
+    auto d1f2 = make_hash(d1, f2);
+    auto d1f3 = make_hash(d1, f3);
+
+    auto d2f1 = make_hash(d2, f1);
+    auto d2f2 = make_hash(d2, f2);
+    auto d2f3 = make_hash(d2, f3);
+
+    auto d3f1 = make_hash(d3, f1);
+    auto d3f2 = make_hash(d3, f2);
+    auto d3f3 = make_hash(d3, f3);
+
+    auto all_hashes = {d1f1, d1f2, d1f3, d2f1, d2f2, d2f3, d3f1, d3f2, d3f3};
+    std::cout << "Size of all_hashes: " << all_hashes.size() << "\n";
+    for (auto const& name : all_hashes) {
+      std::cout << name << std::endl;
+    }
+    std::set<decltype(d1f1)> duplicate_less(all_hashes.begin(),
+                                            all_hashes.end());
+    CHECK(all_hashes.size() == duplicate_less.size());
+  }
+}
+TEST_SUITE_END();  // Learning tests
--- tdd-samples/callback-infra/src/implementation.h
+++ tdd-samples/callback-infra/src/implementation.h
@@ -1,9 +1,16 @@
 #include <future>
 #include <iostream>
+#include <typeinfo>
 
+#include "solution.h"
+
+#ifndef _CALLBACK_INFRA_IMPLEMENTATION_
+
+#define _CALLBACK_INFRA_IMPLEMENTATION_
 class Worker {
-public:
+ public:
   virtual bool operator==(Worker const &other) const { return true; }
+  virtual bool operator!=(Worker const &other) const { return false; }
   virtual void schedule(Duration period, CallbackFunction callback) {
     throw std::runtime_error("Not Implemented!");
   }
@@ -12,7 +19,7 @@ public:
 };
 
 class CallbackInfrastructureImpl : public CallbackInfrastructure {
-public:
+ public:
   CallbackInfrastructureImpl() = default;
   using FactoryMethodType = std::function<Worker *()>;
   CallbackInfrastructureImpl(FactoryMethodType factory) : factory_{factory} {}
@@ -22,53 +29,41 @@ public:
     return 0;
   };
 
-private:
+ private:
   Worker *worker_;
   FactoryMethodType factory_;
 };
 
-class Infomap {
-public:
-  int add(Worker const &w) { throw std::runtime_error("Not Implemented"); }
-  Worker remove(int id) { throw std::runtime_error("Not Implemented"); };
-};
-
-class AsyncWorker
-{
-  public:
-    void schedule(Duration period, CallbackFunction callback) {
-        worker_fut = std::async([=](){
-          while(!stop)
-          {
-            std::this_thread::sleep_for(period);
-            if(!stop)
-            {
-              callback();
-            }
-          }
-        });
-    }
-    void cancel() {
-      if(worker_fut.valid())
-      {
-        std::cout << " cancel" << std::endl;
-        stop = true;
-        worker_fut.get();
+class AsyncWorker {
+ public:
+  void schedule(Duration period, CallbackFunction callback) {
+    worker_fut = std::async([=]() {
+      while (!stop) {
+        std::this_thread::sleep_for(period);
+        if (!stop) {
+          callback();
+        }
       }
+    });
+  }
+  void cancel() {
+    if (worker_fut.valid()) {
+      std::cout << " cancel" << std::endl;
+      stop = true;
+      worker_fut.get();
     }
+  }
 
-    ~AsyncWorker()
-    {
-      cancel();
-    }
+  ~AsyncWorker() { cancel(); }
 
-  private:
-    std::future<void> worker_fut;
-    bool stop {false};
+ private:
+  std::future<void> worker_fut;
+  bool stop{false};
 };
 
-template <typename T = AsyncWorker> class WorkerImpl : public Worker {
-public:
+template <typename T = AsyncWorker>
+class WorkerImpl : public Worker {
+ public:
   WorkerImpl() = default;
   WorkerImpl(T &&impl) : impl_(std::forward<T>(impl)) {}
   void schedule(Duration period, CallbackFunction callback) override {
@@ -76,6 +71,16 @@ public:
   };
   void cancel() override { impl_.cancel(); };
 
-private:
+ private:
   T impl_;
 };
+
+CallbackInfrastructure::IdType MakeId(Duration period, CallbackFunction cb) {
+  return std::hash<std::string>{}(std::to_string(period.count()) +
+                                  typeid(cb).name());
+}
+
+// TODO: Put the Duration, Instant types in namespace and move the MakeId and
+// Now() functions there.
+
+#endif
