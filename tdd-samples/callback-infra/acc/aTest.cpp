7f1b331b050ed55ed75068500b56ce82c4004899
koushik-ms <koushik.ms@gmail.com>
2020-04-28 22:58:30 +0200

    add acceptance test for valid ticks

--- tdd-samples/callback-infra/acc/aTest.cpp
+++ tdd-samples/callback-infra/acc/aTest.cpp
@@ -28,10 +28,9 @@ TEST_CASE("CInShd allow registration of timed callbacks" *
   CHECK(latency >= (period - tick));
 }
 
-TEST_CASE(
-    "CInShd allow registration and deregistration. Call repeatedly until "
-    "deregistration" *
-    doctest::skip(SKIP)) {
+TEST_CASE("CInShd allow registration and deregistration. Call repeatedly until "
+          "deregistration" *
+          doctest::skip(SKIP)) {
   using namespace std::chrono_literals;
   auto period = 500ms;
   auto tick = 50ms;
@@ -68,10 +67,10 @@ TEST_CASE(
   CHECK(sizeAfterDereg == snapshots.size());
   CHECK(minLatency >= (period - tick));
   CHECK(minLatency <
-        (period + tick));  // make sure this is not the value init'ed
+        (period + tick)); // make sure this is not the value init'ed
   CHECK(maxLatency <= (period + tick));
   CHECK(maxLatency >
-        (period - tick));  // make sure this is not the value init'ed
+        (period - tick)); // make sure this is not the value init'ed
 }
 
 TEST_CASE("CInShd start calling after registration" * doctest::skip(SKIP) *
@@ -80,6 +79,84 @@ TEST_CASE("CInShd stop calling after de-registration" * doctest::skip(SKIP) *
           doctest::may_fail()) {}
 TEST_CASE(
     "CInShd shall call within period +/- clock-tick for various clock-ticks" *
-    doctest::skip(SKIP) * doctest::may_fail()) {}
+    doctest::skip(SKIP)) {
+  using namespace std::chrono_literals;
+  Duration period;
+  Duration tick;
+  SUBCASE("") {
+    period = 100ms;
+    tick = 1ms;
+  }
+  SUBCASE("") {
+    period = 50ms;
+    tick = 10ms;
+  }
+  SUBCASE("") {
+    period = 100ms;
+    tick = 100ms;
+  }
+
+  CAPTURE(period.count());
+  CAPTURE(tick.count());
+
+  Instant registeredAt, calledAt;
+  auto builder = getCallbackInfrastructureBuilder();
+  auto cIn = builder->Build();
+
+  auto callbackId =
+      cIn->registerCallback(period, [&calledAt]() { calledAt = Now(); });
+  registeredAt = Now();
+
+  std::this_thread::sleep_for(period + tick);
+  cIn->deregisterCallback(callbackId);
+  auto latency = calledAt - registeredAt;
+  std::cout << "Period: " << period.count() << " tick: " << tick.count()
+            << " ra: " << registeredAt.time_since_epoch().count()
+            << " ca: " << calledAt.time_since_epoch().count()
+            << " latency: " << latency.count() << std::endl;
+  CHECK(latency <= (period + tick));
+  CHECK(latency >= (period - tick));
+}
+
+TEST_CASE("CInShd shall fail to work for too small values of clock-ticks" *
+          doctest::skip(SKIP) * doctest::should_fail()) {
+  using namespace std::chrono_literals;
+  Duration period;
+  Duration tick;
+  SUBCASE("") {
+    period = 10ms;
+    tick = 100us;
+  }
+  SUBCASE("") {
+    period = 5ms;
+    tick = 10us;
+  }
+  SUBCASE("") {
+    period = 1ms;
+    tick = 1us;
+  }
+
+  CAPTURE(period.count());
+  CAPTURE(tick.count());
+
+  Instant registeredAt, calledAt;
+  auto builder = getCallbackInfrastructureBuilder();
+  auto cIn = builder->Build();
+
+  auto callbackId =
+      cIn->registerCallback(period, [&calledAt]() { calledAt = Now(); });
+  registeredAt = Now();
+
+  std::this_thread::sleep_for(period + tick);
+  cIn->deregisterCallback(callbackId);
+  auto latency = calledAt - registeredAt;
+  std::cout << "Period: " << period.count() << " tick: " << tick.count()
+            << " ra: " << registeredAt.time_since_epoch().count()
+            << " ca: " << calledAt.time_since_epoch().count()
+            << " latency: " << latency.count() << std::endl;
+  CHECK(latency <= (period + tick));
+  CHECK(latency >= (period - tick));
+}
+
 TEST_CASE("CInShd disallow registration when period is not multiple of tick" *
           doctest::skip(SKIP) * doctest::may_fail()) {}
